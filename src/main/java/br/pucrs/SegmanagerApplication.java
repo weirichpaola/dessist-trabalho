b6c909627a9a60d9e952ad8453f0f71626d2f1a4
Gustavo Kath <gustavo.kath@acad.pucrs.br>
2017-04-14 19:01:01 -0300

    Spring init

---
+++ src/main/java/br/pucrs/SegmanagerApplication.java
@@ -0,0 +1,12 @@
+package br.pucrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegmanagerApplication.class, args);
+	}
+}
